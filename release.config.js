09ef21d9578dbfd063b9d6ec197b4eba68a47899
Andrea Acampora <andrea.acampora@protonmail.com>
2024-08-19 16:01:07 +0200

    fix: change tag format

--- release.config.js
+++ release.config.js
@@ -1,11 +1,11 @@
 const publishCommands = `
-git tag -a -f \${nextRelease.version} -F CHANGELOG.md || exit 2
 git push --force origin \${nextRelease.version} || exit 3
 echo "release_status=released" >> $GITHUB_ENV
 `;
 
 module.exports = {
   branches: ['master'],
+  tagFormat: '${version}',
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
@@ -15,7 +15,6 @@ module.exports = {
       path: '@semantic-release/git',
       assets: ['CHANGELOG.md', 'package.json'],
       message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-      tagFormat: '${version}',
     },
     {
       path: '@semantic-release/exec',
