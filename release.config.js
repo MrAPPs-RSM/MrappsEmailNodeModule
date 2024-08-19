3c9175c5379683167c6c025be9548c520e4c8aff
Andrea Acampora <andrea.acampora@protonmail.com>
2024-08-19 15:28:18 +0200

    fix: change version pattern

---
+++ release.config.js
@@ -0,0 +1,25 @@
+const publishCommands = `
+git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md || exit 2
+git push --force origin \${nextRelease.version} || exit 3
+echo "release_status=released" >> $GITHUB_ENV
+`;
+
+module.exports = {
+  branches: ['master'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/github',
+    {
+      path: '@semantic-release/git',
+      assets: ['CHANGELOG.md', 'package.json'],
+      message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+    },
+    '@semantic-release/exec',
+    {
+      path: '@semantic-release/exec',
+      publishCmd: publishCommands
+    },
+  ],
+};
